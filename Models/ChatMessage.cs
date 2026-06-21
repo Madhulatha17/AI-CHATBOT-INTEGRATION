335a6f5861f53ec29cdc9aa84b5733e332f3da2d
Madhulatha17 <bandimadhulatha4@gmail.com>
2026-06-21 22:46:16 +0530

    Create ChatMessage.cs

---
+++ Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+namespace AIChatbotIntegration.Models
+{
+    public class ChatMessage
+    {
+        public string UserMessage { get; set; }
+
+        public string BotResponse { get; set; }
+    }
+}
